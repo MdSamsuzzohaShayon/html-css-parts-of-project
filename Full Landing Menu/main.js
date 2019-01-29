b9717045915edd4235fb41da639c105309fb7955
Md Shayon <mdshayon0@gmail.com>
2019-01-29 17:29:07 +0600

    Initial code. A completed project made by Me.

---
+++ Full Landing Menu/main.js
@@ -0,0 +1,27 @@
+let show = false;
+
+const menu_icon = document.getElementById('icon');
+const close_btn = document.getElementById("close-btn");
+const full_menu = document.getElementById("full-menu");
+const showcase = document.getElementById("showcase");
+
+
+
+
+menu_icon.addEventListener('click', () => {
+    show = true;
+    if (show) {
+        full_menu.style.display = 'block';
+        setTimeout(() => {
+            showcase.style.display = 'none';
+        }, 1000);
+    }
+});
+
+close_btn.addEventListener('click', () => {
+    if (show) {
+        showcase.style.display = 'block';
+        full_menu.style.display = 'none';
+    }
+
+})
