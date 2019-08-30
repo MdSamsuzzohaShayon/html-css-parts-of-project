10fe2fcdd151f30aef6d4df6d0f32d6550e8516b
Md Shayon <mdshayon0@gmail.com>
2019-08-30 17:09:43 +0600

    js problem fixed

--- Photography/js/main.js
+++ Photography/js/main.js
@@ -1,17 +1,31 @@
 const image = document.querySelectorAll('.image');
 const image_output = document.getElementById('image-output');
 console.log(image_output.src);
-let image_output_src = image_output.src;
+// let image_output_src = image_output.src;
 
 // image_output.innerText.src = 
 
 
-image.forEach(img => img.addEventListener('click', e =>{
-        console.log(e.target.attributes);
+
+
+
+
+
+image.forEach(img => {
+    img.addEventListener('click', e =>{
+        // console.log(e.target.attributes);
         console.log(e.target.src);       
         // const image_src = e.target.src;
         
-        image_output_src = e.target.src;
+        // image_output_src = e.target.src;
+        // b.setAttribute("name", "helloButton");
+        image_output.setAttribute('src', e.target.src);
+
+
+        // console.log(image_output_src);
+        // let out = document.getElementById('output');
+        // out.innerHTML = image_output_src;
+        
 
         // image_output.innerText.src = image_src ;
         // let img = document.createElement("img").classList.add("ui" , "large", "image")
@@ -19,6 +33,6 @@ image.forEach(img => img.addEventListener('click', e =>{
 
         // image_output.childNodes
         
-    }) 
-);
+    }) ;
+});
 
